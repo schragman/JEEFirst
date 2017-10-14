5b0c12548ad69cb466ffc7820f563ff8ba3765fe
Captain <Captain@Voyager.fritz.box>
2017-10-15 00:45:31 +0200

    First Commit

---
+++ my-Aktion/src/main/java/de/dpunkt/myaktion/controller/EditCampaignController.java
@@ -0,0 +1,34 @@
+package de.dpunkt.myaktion.controller;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import de.dpunkt.myaktion.data.CampaignListProducer;
+import de.dpunkt.myaktion.data.CampaignProducer;
+
+
+@SessionScoped
+@Named
+public class EditCampaignController implements Serializable{
+
+	private static final long serialVersionUID = 2815796004558360299L;
+	
+	@Inject
+	private CampaignListProducer campaignListProducer;
+	@Inject
+	private CampaignProducer campaignProducer;
+	
+	public String doSave() {
+		if (campaignProducer.isAddMode()) {
+			campaignListProducer.getCampaigns().add(campaignProducer.getSelectedCampaign());
+		}
+		return Pages.LIST_CAMPAIGNS;
+	}
+	
+	public String doCancel() {
+		return Pages.LIST_CAMPAIGNS;
+	}
+}
---
+++ my-Aktion/src/main/java/de/dpunkt/myaktion/controller/ListCampaignsController.java
@@ -0,0 +1,46 @@
+package de.dpunkt.myaktion.controller;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import de.dpunkt.myaktion.data.CampaignProducer;
+import de.dpunkt.myaktion.model.Campaign;
+
+@SessionScoped
+@Named
+
+public class ListCampaignsController implements Serializable{
+
+	@Inject
+	private CampaignProducer campaignProducer;
+	
+	private static final long serialVersionUID = 869327738364802522L;
+	
+	public String doAddCampaign() {
+		campaignProducer.prepareAddCampaign();
+		return Pages.EDIT_CAMPAIGN;
+	}
+	
+	public String doEditCampaign(Campaign campaign) {
+		campaignProducer.prepareEditCampaign(campaign);
+		return Pages.EDIT_CAMPAIGN;
+	}
+	
+	public String doEditDonationForm(Campaign campaign) {
+		campaignProducer.setSelectedCampaign(campaign);
+		return Pages.EDIT_DONATION_FORM;
+	}
+	
+	public String doListDonations(Campaign campaign) {
+		campaignProducer.setSelectedCampaign(campaign);
+		return Pages.LIST_DONATIONS;
+	}
+	
+	public void doDeleteCampaign(Campaign campaign) {
+		System.out.println("Deletion not implemented, yet!");
+	}
+
+}
