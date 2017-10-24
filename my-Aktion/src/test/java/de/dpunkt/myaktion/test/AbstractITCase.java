d9a51eafeca808e719ecccbf470f627c8fa93389
Captain <Captain@Voyager.fritz.box>
2017-10-24 12:35:58 +0200

    included first test-classes

---
+++ my-Aktion/src/test/java/de/dpunkt/myaktion/test/AbstractITCase.java
@@ -0,0 +1,24 @@
+package de.dpunkt.myaktion.test;
+
+//import de.dpunkt.myaktion.test.pages.LoginPage;
+import org.jboss.arquillian.container.test.api.Deployment;
+//import org.jboss.arquillian.graphene.Graphene;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.importer.ZipImporter;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+
+import java.io.File;
+
+public class AbstractITCase {
+	@Deployment(testable = false)
+    public static WebArchive createDeployment() {
+        WebArchive archive = ShrinkWrap.create(ZipImporter.class, "test.war").importFrom(new File("target/my-aktion.war"))
+                .as(WebArchive.class);
+        archive.delete("/WEB-INF/classes/META-INF/persistence.xml");
+        archive.delete("/WEB-INF/myaktion-ds.xml");
+        archive.addAsResource("META-INF/test-persistence.xml",
+                "META-INF/persistence.xml");
+        archive.addAsWebInfResource("test-ds.xml", "test-ds.xml");
+        return archive;
+    }
+}
